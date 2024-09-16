420506ba1db00eaccf1b69e42a6bbcb1c7635de3
Yangwook Jeong <yangwookee@gmail.com>
2024-09-16 15:21:38 +0900

    feat: add hook for loading csv

---
+++ src/hooks/useFetchCsv.ts
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react'
+import Papa from 'papaparse'
+
+export const useFetchCsv = (url: string) => {
+  const [header, setHeader] = useState<string[]>()
+  const [rows, setRows] = useState<string[][]>([])
+  const [error, setError] = useState<Error | null>(null)
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.text())
+      .then((text) => {
+        const parsedData = Papa.parse<string[]>(text)
+        setHeader(parsedData.data[0])
+        setRows(parsedData.data.slice(1))
+      })
+      .catch((error) => setError(error))
+  }, [url])
+
+  return { header, rows, error }
+}
