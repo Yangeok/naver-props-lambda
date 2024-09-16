c859f175f60b1fd90113d720b35a8f7d9b29855d
Yangwook Jeong <yangwookee@gmail.com>
2024-09-16 14:23:28 +0900

    refactor: refactor marker component

--- src/components/MarkerContent.tsx
+++ src/components/MarkerContent.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 
 interface MarkerContentProps {
   title: string;
@@ -35,24 +35,22 @@ const MarkerContent: React.FC<MarkerContentProps> = ({
   date,
   firstDate,
 }) => {
+  const renderLink = (url: string, altText: string, iconUrl: string) => (
+    <a href={url} target="_blank" rel="noopener noreferrer">
+      <img
+        src={iconUrl}
+        style={{ verticalAlign: 'bottom', width: '24px', height: '24px' }}
+        alt={altText}
+      />
+    </a>
+  );
+
   return (
     <div style={{ width: '400px' }}>
       <div>
         {title} {amount}억 ({approvalYear}년 승인)
-        <a href={link1} target="_blank" rel="noopener noreferrer">
-          <img
-            src="https://t1.daumcdn.net/localimg/localimages/07/common/kakaomap_favicon.ico"
-            style={{ verticalAlign: 'bottom', width: '24px', height: '24px' }}
-            alt="Kakao Map Favicon"
-          />
-        </a>
-        <a href={link2} target="_blank" rel="noopener noreferrer">
-          <img
-            src="https://www.naver.com/favicon.ico"
-            style={{ verticalAlign: 'bottom', width: '24px', height: '24px' }}
-            alt="Naver Favicon"
-          />
-        </a>
+        {renderLink(link1, "Kakao Map Favicon", "https://t1.daumcdn.net/localimg/localimages/07/common/kakaomap_favicon.ico")}
+        {renderLink(link2, "Naver Favicon", "https://www.naver.com/favicon.ico")}
       </div>
       <div>
         {area} {size}m² {approvalYear} {floorInfo}세대
@@ -69,7 +67,7 @@ const MarkerContent: React.FC<MarkerContentProps> = ({
         {firstDate && date !== firstDate ? ` (${firstDate} 최초 등록)` : ''}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default MarkerContent;
+export default MarkerContent
