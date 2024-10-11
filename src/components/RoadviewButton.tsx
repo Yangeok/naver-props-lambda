f718162d921e050b0b73830191a35a7db2971e42
Yangwook Jeong <yangwookee@gmail.com>
2024-10-11 23:07:05 +0900

    fix: fix roadview button

--- src/components/RoadviewButton.tsx
+++ src/components/RoadviewButton.tsx
@@ -7,14 +7,18 @@ interface IRoadviewButton {
 
 export const RoadviewButton: React.FC<IRoadviewButton> = ({ isVisible, onClick }) => {
   const sourceImage = 'https://t1.daumcdn.net/localimg/localimages/07/2018/pc/common/img_search.png'
-  const enabled = 'bg-[0_-350px]'
-  const disabled = 'bg-[0_-450px]'
+  const enabled = '0 -350px'
+  const disabled = '0 -450px'
 
   return (
     <div
       role="button"
       onClick={onClick}
-      className={`absolute top-1 left-1 w-10 h-10 z-10 cursor-pointer bg-no-repeat bg-[url('${sourceImage}')] ${isVisible ? enabled : disabled}`}
+      className="absolute z-10 w-10 h-10 bg-no-repeat cursor-pointer top-1 left-1"
+      style={{
+        backgroundImage: `url('${sourceImage}')`,
+        backgroundPosition: isVisible ? enabled : disabled,
+      }}
     />
   )
 }
