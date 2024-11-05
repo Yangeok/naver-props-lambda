93cc7d7432df8f7357cdde5e2eda24dbd7b53a78
Yangwook Jeong <yangwookee@gmail.com>
2024-11-05 12:47:55 +0900

    feat: add zoom management component

--- src/MapSection.tsx
+++ src/MapSection.tsx
@@ -5,18 +5,15 @@ import {
   CustomOverlayMap,
   Map,
   MapMarker,
-  MapTypeControl,
   MapTypeId,
-  ZoomControl,
 } from 'react-kakao-maps-sdk'
 
-import { MapMenu, MarkerContent, MarkerHeader } from './components'
+import { MarkerContent, MarkerHeader, ZoomControl } from './components'
 import './index.css'
 import './MapWalker.css'
 import {
   Center,
   DataItem,
-  MapTypeIdEnum,
   MarkerData,
   getLatestDate,
   getMarkerImageSrc,
@@ -74,11 +71,25 @@ export const MapSection: React.FC<MapSectionProps> = ({
     return index !== undefined ? "m" + index : ""
   }
 
+  const zoomIn = () => {
+    const map = mapRef.current
+    if (!map) return
+    // map.setLevel(map.getLevel() - 1) // REMOVE:
+    setZoomLevel(map.getLevel() - 1)
+  }
+
+  const zoomOut = () => {
+    const map = mapRef.current
+    if (!map) return
+    // map.setLevel(map.getLevel() + 1) // REMOVE:
+    setZoomLevel(map.getLevel() + 1)
+  }
+
   const markers = useMemo(() => generateMarkers(data, selectedMarker, setSelectedMarker), [data, selectedMarker])
 
   useEffect(() => {
     if (!mapRef.current) return
-    
+
     if (isRoadviewVisible) {
       mapRef.current.addOverlayMapTypeId(kakao.maps.MapTypeId.ROADVIEW)
       setCenter({
@@ -91,13 +102,13 @@ export const MapSection: React.FC<MapSectionProps> = ({
       mapRef.current.removeOverlayMapTypeId(kakao.maps.MapTypeId.ROADVIEW)
       setSizes(['100%', '0'])
     }
-    
+
     mapRef.current.relayout()
   }, [mapRef, isRoadviewVisible])
 
   useEffect(() => {
     if (!mapRef.current) return
-  
+
     mapRef.current.relayout()
   }, [sizes])
 
@@ -129,13 +140,14 @@ export const MapSection: React.FC<MapSectionProps> = ({
         </>
       )}
 
-      {/* Map Controls */}
-      <MapMenu setMapTypeIds={handleSetMapTypeIds} mapTypeIds={mapTypeIds} />
-      <MapTypeControl position="TOPRIGHT" />
-      <ZoomControl position="RIGHT" />
+      {/* REMOVE:Map Controls */}
+      {/* <MapMenu setMapTypeIds={handleSetMapTypeIds} mapTypeIds={mapTypeIds} /> */}
 
-      {/* Map Types */}
-      {mapTypeIds && mapTypeIds.map(mapTypeId => <MapTypeId type={mapTypeId} key={mapTypeId} />)}
+      {/* Zoom Control */}
+      <ZoomControl onZoomIn={zoomIn} onZoomOut={zoomOut} />
+
+      {/* REMOVE: Map Types */}
+      {/* {mapTypeIds && mapTypeIds.map(mapTypeId => <MapTypeId type={mapTypeId} key={mapTypeId} />)} */}
 
       {/* Render Markers */}
       {markers}
---
+++ src/components/ZoomControl.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+interface ZoomControlProps {
+  onZoomIn: () => void
+  onZoomOut: () => void
+}
+
+export const ZoomControl: React.FC<ZoomControlProps> = ({ onZoomIn, onZoomOut }) => {
+  return (
+    <div className="absolute top-[50px] right-[10px] w-[36px] h-[80px] overflow-hidden z-[1] bg-[#f5f5f5] border border-[#bfbfbf] rounded shadow-md">
+      <span
+        onClick={onZoomIn}
+        className="block w-[36px] h-[40px] text-center cursor-pointer text-xl font-bold leading-[40px] border-b border-[#bfbfbf] hover:bg-[#e6e6e6]"
+      >
+        +
+      </span>
+      <span
+        onClick={onZoomOut}
+        className="block w-[36px] h-[40px] text-center cursor-pointer text-xl font-bold leading-[40px] hover:bg-[#e6e6e6]"
+      >
+        -
+      </span>
+    </div>
+  )
+}
